a96f5abaf1aca351759f79bb6225ff49124ef6fc
12Andy12 <saw271002@mail.ru>
2021-05-06 18:53:00 +0700

    Add top panel with function(Minimize and cloze program)

--- src/MainForm.Designer.cs
+++ src/MainForm.Designer.cs
@@ -31,7 +31,13 @@ namespace Vs_Progect_Tariff
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.MinimizeButton = new System.Windows.Forms.Button();
+            this.IconPicture = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.MainPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.IconPicture)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -44,6 +50,68 @@ namespace Vs_Progect_Tariff
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.CloseButton.Cursor = System.Windows.Forms.Cursors.Default;
+            this.CloseButton.FlatAppearance.BorderSize = 0;
+            this.CloseButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Maroon;
+            this.CloseButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.CloseButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CloseButton.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CloseButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.CloseButton.Location = new System.Drawing.Point(444, 0);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(56, 31);
+            this.CloseButton.TabIndex = 1;
+            this.CloseButton.Text = "X";
+            this.CloseButton.UseVisualStyleBackColor = false;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.BackColor = System.Drawing.Color.Black;
+            this.MainPanel.Controls.Add(this.MinimizeButton);
+            this.MainPanel.Controls.Add(this.IconPicture);
+            this.MainPanel.Controls.Add(this.CloseButton);
+            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.MainPanel.Location = new System.Drawing.Point(0, 0);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(500, 31);
+            this.MainPanel.TabIndex = 2;
+            this.MainPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainPanel_MouseDown);
+            this.MainPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainPanel_MouseMove);
+            // 
+            // MinimizeButton
+            // 
+            this.MinimizeButton.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.MinimizeButton.Cursor = System.Windows.Forms.Cursors.Default;
+            this.MinimizeButton.FlatAppearance.BorderSize = 0;
+            this.MinimizeButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Maroon;
+            this.MinimizeButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.MinimizeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.MinimizeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MinimizeButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.MinimizeButton.Location = new System.Drawing.Point(391, 0);
+            this.MinimizeButton.Name = "MinimizeButton";
+            this.MinimizeButton.Size = new System.Drawing.Size(56, 31);
+            this.MinimizeButton.TabIndex = 4;
+            this.MinimizeButton.Text = "-";
+            this.MinimizeButton.UseVisualStyleBackColor = false;
+            this.MinimizeButton.Click += new System.EventHandler(this.MinimizeButton_Click);
+            // 
+            // IconPicture
+            // 
+            this.IconPicture.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.IconPicture.Image = ((System.Drawing.Image)(resources.GetObject("IconPicture.Image")));
+            this.IconPicture.Location = new System.Drawing.Point(3, 0);
+            this.IconPicture.Name = "IconPicture";
+            this.IconPicture.Size = new System.Drawing.Size(19, 31);
+            this.IconPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.IconPicture.TabIndex = 3;
+            this.IconPicture.TabStop = false;
             // 
             // MainForm
             // 
@@ -51,12 +119,18 @@ namespace Vs_Progect_Tariff
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(484, 311);
+            this.ClientSize = new System.Drawing.Size(500, 350);
+            this.ControlBox = false;
+            this.Controls.Add(this.MainPanel);
             this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form2_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.MainPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.IconPicture)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -64,6 +138,10 @@ namespace Vs_Progect_Tariff
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.PictureBox IconPicture;
+        private System.Windows.Forms.Button MinimizeButton;
     }
 }
 
--- src/MainForm.cs
+++ src/MainForm.cs
@@ -21,5 +21,35 @@ namespace Vs_Progect_Tariff
         {
             pictureBox1.BackColor = Color.Transparent;
         }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        Point LastPoint;
+        private void MainPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - LastPoint.X;
+                this.Top += e.Y - LastPoint.Y;
+            }
+        }
+
+        private void MainPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            LastPoint = new Point(e.X, e.Y);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Опа");
+        }
+
+        private void MinimizeButton_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
     }
 }
