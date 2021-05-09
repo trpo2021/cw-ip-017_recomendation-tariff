1724d076d6868025925c10fb3cc6cf1168d85fc5
12Andy12 <saw271002@mail.ru>
2021-05-09 18:12:39 +0700

    Add ClassTariffs

---
+++ src/ClassTariffs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vs_Progect_Tariff
+{
+    class ClassTariffs
+    {
+        public string mobileOperator;
+        public string name;
+        public string money;
+        public string gigi;
+        public string minutes;
+    }
+}
