4fd37d5d170f158a0ed14060a170a57837481bf8
12Andy12 <saw271002@mail.ru>
2021-06-13 22:56:59 +0700

    Add Test for isNumber

--- src/Program.cs
+++ src/Program.cs
@@ -23,5 +23,5 @@ namespace Project
 
     }
 
-    
+
 }
---
+++ src/Tests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static Project.logic;
+namespace Project
+{
+    class Tests
+    {
+        public static int CountTestsPassed = 0;
+        public static int CountTestsFailed = 0;
+        public static bool BOOL_TEST(string nameTest, Func<string, bool> testMethod, string data, bool value)
+        {
+            bool res = testMethod(data);
+            if (res != value)
+            {
+                System.Console.Write($"{nameTest}  crash. With a value of {data}, expected the {value}, got the {res}\n");
+                CountTestsFailed++;
+                return false;
+            }
+            CountTestsPassed++;
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            BOOL_TEST("test isNumber", isNumber, "1231", true);
+            BOOL_TEST("test isNumber", isNumber, "asd", false);
+            BOOL_TEST("test isNumber", isNumber, "asd123", false);
+            System.Console.Write($"Completed tests: {CountTestsPassed}\tFailed Tests: {CountTestsFailed}");
+            Thread.Sleep(3000);
+        }
+    }
+}
